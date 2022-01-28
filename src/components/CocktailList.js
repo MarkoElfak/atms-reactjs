264273f169dab2b933c7e3fa9affb2e587083f1b
MareVR <marko.max.ilic97@gmail.com>
2022-01-28 21:00:11 +0100

    Context/AppProvider

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -4,6 +4,15 @@ import Loading from "./Loading";
 import { useGlobalContext } from "../context";
 
 const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext();
+
+  if (loading) {
+    return <Loading />;
+  }
+  if (cocktails.length < 1) {
+    return <h2 className="section-title"> Cocktail not found </h2>;
+  }
+
   return (
     <div>
       <h2>cocktail list</h2>

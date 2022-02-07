3d1b519af56844c47580317f7776a6e7ea487ffc
MareVR <marko.max.ilic97@gmail.com>
2022-02-07 22:48:19 +0100

    Search Form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,10 +3,36 @@ import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
+
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  const handleSubmit = (e) => {
+    //not to refresh search page if nothing has changed
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name"> search for cocktail:</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
   );
 };
 
